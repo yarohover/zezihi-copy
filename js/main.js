2ca872ebe4c21f90fbaf85d4bcb68f5808b86a13
yaro <skhn00j@gmail.com>
2022-05-03 17:00:20 +0900

    publish article number color change javascript done

--- js/main.js
+++ js/main.js
@@ -1,12 +1,50 @@
+// publishing projects number effect
+let $pubs = document.querySelectorAll('#publish article');
+function pubNum(winY) {
+    if ($pubs[0].offsetTop < winY && winY < $pubs[1].offsetTop) {
+        $pubs[0].classList.add('active');
+        $pubs[1].classList.remove('active');
+        $pubs[2].classList.remove('active');
+        $pubs[3].classList.remove('active');
+        $pubs[4].classList.remove('active');
+    } else if ($pubs[1].offsetTop < winY && winY < $pubs[2].offsetTop) {
+        $pubs[1].classList.add('active');
+        $pubs[0].classList.remove('active');
+        $pubs[2].classList.remove('active');
+        $pubs[3].classList.remove('active');
+        $pubs[4].classList.remove('active');
+    } else if ($pubs[2].offsetTop < winY && winY < $pubs[3].offsetTop) {
+        $pubs[2].classList.add('active');
+        $pubs[0].classList.remove('active');
+        $pubs[1].classList.remove('active');
+        $pubs[3].classList.remove('active');
+        $pubs[4].classList.remove('active');
+    } else if ($pubs[3].offsetTop < winY && winY < $pubs[4].offsetTop) {
+        $pubs[3].classList.add('active');
+        $pubs[0].classList.remove('active');
+        $pubs[1].classList.remove('active');
+        $pubs[2].classList.remove('active');
+        $pubs[4].classList.remove('active');
+    } else if ($pubs[4].offsetTop < winY) {
+        $pubs[4].classList.add('active');
+        $pubs[0].classList.remove('active');
+        $pubs[1].classList.remove('active');
+        $pubs[2].classList.remove('active');
+        $pubs[3].classList.remove('active');
+    }
+}
+
+
 // second section scroll effect
 let $script_start = document.querySelector('#second_sec').offsetTop;
-console.log($script_start);
 let $script_wrap = document.querySelector('#second_sec #wrap2');
-window.addEventListener('scroll', function(event){
+
+// window scroll event
+window.addEventListener('scroll', function(){
     let winY = window.scrollY;
+    pubNum(winY);
     if ($script_start < winY) {
         let gap = winY - $script_start;
-        console.log(gap);
         $script_wrap.scrollTo(gap,0);
     }
 });
